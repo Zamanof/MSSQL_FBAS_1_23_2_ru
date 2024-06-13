ad195c6e30e286d3ac1e7ca376a9d75c8be23896
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2024-06-13 16:19:02 +0400

    Aggregate v2

--- MSSQL 03. Aggregate functions.sql
+++ MSSQL 03. Aggregate functions.sql
@@ -1,7 +1,14 @@
 -- Aggregate functions
 -- https://learn.microsoft.com/en-us/sql/t-sql/functions/aggregate-functions-transact-sql?view=sql-server-ver16
 
+SELECT *
+FROM Books_En
+
 -- SUM(column_name) - возвращает сумму указанного столбца
 
 SELECT SUM(Pages) AS AllPageSum
 FROM Books_En
+
+SELECT SUM(Pages) AS TutorialsPageSum
+FROM Books_En
+WHERE Category LIKE N'Tutorials'
