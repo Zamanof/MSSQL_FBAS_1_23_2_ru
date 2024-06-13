21f054385f3cc5b4cac5a2b9ddf88873560c1284
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2024-06-13 16:01:16 +0400

    Aggregate

---
+++ MSSQL 03. Aggregate functions.sql
@@ -0,0 +1,4 @@
+-- Aggregate functions
+-- https://learn.microsoft.com/en-us/sql/t-sql/functions/aggregate-functions-transact-sql?view=sql-server-ver16
+
+-- SUM(column_name) - возвращает сумму указанного столбца
