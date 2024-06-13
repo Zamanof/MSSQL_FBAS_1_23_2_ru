df9ca34b4cf66beead7c3d690d310f82b06a462c
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2024-06-13 16:05:43 +0400

    Aggregate v1

--- MSSQL 03. Aggregate functions.sql
+++ MSSQL 03. Aggregate functions.sql
@@ -2,3 +2,6 @@
 -- https://learn.microsoft.com/en-us/sql/t-sql/functions/aggregate-functions-transact-sql?view=sql-server-ver16
 
 -- SUM(column_name) - возвращает сумму указанного столбца
+
+SELECT SUM(Pages) AS AllPageSum
+FROM Books_En
