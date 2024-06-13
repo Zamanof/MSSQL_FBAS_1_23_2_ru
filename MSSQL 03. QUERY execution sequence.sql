d5d03eebd3f6c277bbd77e58295f4ec0b937667f
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2024-06-13 17:28:33 +0400

    execution sequence

---
+++ MSSQL 03. QUERY execution sequence.sql
@@ -0,0 +1,18 @@
+/*
+Sintakticheskaya posledovatelnost zaprosov:
+	1. SELECT
+	2. FROM
+	3. WHERE
+	4. GROUP BY
+	5. HAVING
+	6. ORDER BY
+
+Realnaya posledovatelnost zaprosov:
+	1. FROM
+	2. WHERE
+	3. GROUP BY
+	4. HAVING
+	5. SELECT
+	6. ORDER BY
+
+*/
